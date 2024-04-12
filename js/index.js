b2490eb5004ed129affca7eb1a9ffc8cc6219ea3
Ossian Dahlberg <ossiandahlberg@hotmail.com>
2024-04-12 21:48:05 +0200

    wordle progress

--- js/index.js
+++ js/index.js
@@ -13,112 +13,109 @@
 // Phone page
 // Guess the villain
 
-coords();
+// coords();
 
-function coords () {
-    navigator.geolocation.getCurrentPosition(
-        show_posistion,
-        null,
-        {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0
-        }
-)};
+// function coords() {
+//     navigator.geolocation.getCurrentPosition(
+//         show_posistion,
+//         null,
+//         {
+//             enableHighAccuracy: true,
+//             timeout: 5000,
+//             maximumAge: 0
+//         }
+//     )
+// };
 
-function show_posistion(position) {
-    let longitude = position.coords.longitude;
-    let latitude = position.coords.latitude;
+// function show_posistion(position) {
+//     let longitude = position.coords.longitude;
+//     let latitude = position.coords.latitude;
 
-    var map = L.map('map').setView([latitude, longitude], 13);
+//     var map = L.map('map').setView([latitude, longitude], 13);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         maxZoom: 19,
+//         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//     }).addTo(map);
 
-    var player_circle = L.circle([latitude, longitude], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 150
-    }).addTo(map);
+//     var player_circle = L.circle([latitude, longitude], {
+//         color: 'red',
+//         fillColor: '#f03',
+//         fillOpacity: 0.5,
+//         radius: 150
+//     }).addTo(map);
 
-    //55.602451, 12.98938
-    var park_circle = L.circle([55.602451, 12.98938], {
-        color: 'green',
-        fillColor: 'lime',
-        fillOpacity: 0.5,
-        radius: 300
-    }).addTo(map);
+//     //55.602451, 12.98938
+//     var park_circle = L.circle([55.602451, 12.98938], {
+//         color: 'green',
+//         fillColor: 'lime',
+//         fillOpacity: 0.5,
+//         radius: 300
+//     }).addTo(map);
 
-    var popup = L.popup();
-    function onMapClick(e) {
-        popup
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(map);
-    }
+//     var popup = L.popup();
+//     function onMapClick(e) {
+//         popup
+//             .setLatLng(e.latlng)
+//             .setContent("You clicked the map at " + e.latlng.toString())
+//             .openOn(map);
+//     }
 
-    map.on('click', onMapClick);
+//     map.on('click', onMapClick);
 
-    check_collision();
+//     check_collision();
 
-    function check_collision() {
-        let x_player = player_circle._path.getBoundingClientRect().x;
-        let y_player = player_circle._path.getBoundingClientRect().y;
-        let width_player = player_circle._path.getBoundingClientRect().width;
-        let height_player = player_circle._path.getBoundingClientRect().height;
-        let right_player = player_circle._path.getBoundingClientRect().right;
-        let bottom_player = player_circle._path.getBoundingClientRect().bottom;
+//     function check_collision() {
+//         let x_player = player_circle._path.getBoundingClientRect().x;
+//         let y_player = player_circle._path.getBoundingClientRect().y;
+//         let right_player = player_circle._path.getBoundingClientRect().right;
+//         let bottom_player = player_circle._path.getBoundingClientRect().bottom;
 
-        let x_park = park_circle._path.getBoundingClientRect().x;
-        let y_park = park_circle._path.getBoundingClientRect().y;
-        let width_park = park_circle._path.getBoundingClientRect().width;
-        let height_park = park_circle._path.getBoundingClientRect().height;
-        let right_park = park_circle._path.getBoundingClientRect().right;
-        let bottom_park = park_circle._path.getBoundingClientRect().bottom;
+//         let x_park = park_circle._path.getBoundingClientRect().x;
+//         let y_park = park_circle._path.getBoundingClientRect().y;
+//         let right_park = park_circle._path.getBoundingClientRect().right;
+//         let bottom_park = park_circle._path.getBoundingClientRect().bottom;
 
-        let bottomInside = false;
-        let topInside = false;
-        let rightInside = false;
-        let leftInside = false;
+//         let bottomInside = false;
+//         let topInside = false;
+//         let rightInside = false;
+//         let leftInside = false;
 
-        if(bottom_player > y_park && bottom_player < bottom_park){
-            bottomInside = true;
-            console.log(bottomInside)
-        }
-        if(x_player < right_park && x_player > x_park){
-            leftInside = true
-            console.log(leftInside)
-        }
-        if(y_player > y_park && y_player < bottom_park){
-            topInside = true
-            console.log(topInside)
-        }
-        if(right_player < right_park && right_player > x_park){
-            rightInside = true
-            console.log(rightInside)
-        }
-    }
+//         if (bottom_player > y_park && bottom_player < bottom_park) {
+//             bottomInside = true;
+//             console.log(bottomInside)
+//         }
+//         if (x_player < right_park && x_player > x_park) {
+//             leftInside = true
+//             console.log(leftInside)
+//         }
+//         if (y_player > y_park && y_player < bottom_park) {
+//             topInside = true
+//             console.log(topInside)
+//         }
+//         if (right_player < right_park && right_player > x_park) {
+//             rightInside = true
+//             console.log(rightInside)
+//         }
+//     }
 
-    navigator.geolocation.watchPosition(update_player_location);
-    
-    function update_player_location(position) {
-        let longitude = position.coords.longitude;
-        let latitude = position.coords.latitude;
+//     navigator.geolocation.watchPosition(update_player_location);
 
-        player_circle.remove();
+//     function update_player_location(position) {
+//         let longitude = position.coords.longitude;
+//         let latitude = position.coords.latitude;
 
-        player_circle = L.circle([latitude, longitude], {
-            color: 'red',
-            fillColor: '#f03',
-            fillOpacity: 0.5,
-            radius: 150
-        }).addTo(map);
+//         player_circle.remove();
 
-        check_collision();
-    }
-}
+//         player_circle = L.circle([latitude, longitude], {
+//             color: 'red',
+//             fillColor: '#f03',
+//             fillOpacity: 0.5,
+//             radius: 150
+//         }).addTo(map);
+
+//         check_collision();
+//     }
+// }
 
 wordle();
--- js/minigames.js
+++ js/minigames.js
@@ -1,14 +1,167 @@
 // Minigames
 
 function wordle() {
-    const main = document.querySelector("main");
+    function createNewRow() {
+        if (document.querySelector(".word-row.active")) {
+            document.querySelector(".word-row.active").classList.remove("active");
+        }
 
-    main.innerHTML = "";
+        const wordRow = document.createElement("div");
+        wordRow.classList.add("word-row", "active")
+
+        for (let i = 0; i < nLetters; i++) {
+            const letterBox = document.createElement("div");
+            letterBox.classList.add("letter-box")
+            letterBox.dataset.boxnum = i;
+
+            if (i == 0) {
+                letterBox.classList.add("active");
+            }
+
+            wordRow.appendChild(letterBox);
+        }
+
+        rowContainer.append(wordRow)
+    }
+
+    function createKeyboard() {
+        const keyboardWrapper = document.createElement("div");
+        keyboardWrapper.classList.add("keyboard-wrapper")
+        gameDiv.append(keyboardWrapper)
+
+        const keyboardContainer = document.createElement("div");
+        keyboardContainer.classList.add("keyboard-container");
+        keyboardWrapper.append(keyboardContainer)
+
+        const qwerty = "QWERTYUIOPÅASDFGHJKLÖÄZXCVBNM"
+
+        const letterArray = [...qwerty]
+
+        letterArray.forEach(letter => {
+            const keyBox = document.createElement("button");
+            keyBox.classList.add("key-box")
+
+            keyBox.dataset.letter = letter;
+
+            keyBox.innerHTML = `
+                <p>${letter}</p>
+            `
+
+            keyBox.addEventListener("click", keyEvent)
+
+            keyboardContainer.append(keyBox)
+        })
+
+        const buttonsContainer = document.createElement("div");
+        buttonsContainer.classList.add("buttons-container");
+
+        const eraseButton = document.createElement("button");
+        eraseButton.classList.add("erase-key")
+        eraseButton.textContent = "⌫"
+        eraseButton.addEventListener("click", eraseEvent)
+
+        const enterButton = document.createElement("button");
+        enterButton.setAttribute("disabled", "true")
+        enterButton.classList.add("enter-key")
+        enterButton.textContent = "ENTER"
+        enterButton.addEventListener("click", enterEvent)
+
+
+        buttonsContainer.append(eraseButton);
+        buttonsContainer.append(enterButton);
+        keyboardWrapper.append(buttonsContainer)
+    }
+
+    function keyEvent(event) {
+        const activeBox = document.querySelector(".letter-box.active");
+        const activeBoxNum = parseInt(activeBox.dataset.boxnum);
+        const activeParent = activeBox.parentElement;
+
+        activeBox.textContent = event.target.dataset.letter;
+
+        activeBox.classList.remove("active");
+
+        if (activeBoxNum != nLetters - 1) {
+            activeParent.childNodes[activeBoxNum + 1].classList.add("active")
+        } else {
+            const allKeys = document.querySelectorAll(".key-box");
+            allKeys.forEach(key => {
+                key.setAttribute("disabled", "true")
+            })
+
+            document.querySelector(".enter-key").removeAttribute("disabled");
+        }
+    }
+
+    function eraseEvent(event) {
+        const activeRow = document.querySelector(".word-row.active");
+        const activeRowBoxes = activeRow.childNodes;
+
+        let anyBoxActive = false;
+        let boxToErase;
+
+        activeRowBoxes.forEach(function (box, index) {
+            if (box.classList.contains("active")) {
+                anyBoxActive = true;
+                if (index != 0) {
+                    boxToErase = activeRowBoxes[index - 1];
+                    box.classList.remove("active");
+                }
+            }
+        })
+
+        if (!anyBoxActive) {
+            boxToErase = activeRowBoxes[nLetters - 1]
+
+            const allKeys = document.querySelectorAll(".key-box");
+            allKeys.forEach(key => {
+                key.removeAttribute("disabled")
+            })
+
+            document.querySelector(".enter-key").setAttribute("disabled", "true");
+        }
+
+        if (boxToErase) {
+            boxToErase.classList.add("active");
+            boxToErase.textContent = "";
+        }
+
+    }
+
+    function enterEvent(event) {
+        const activeRow = document.querySelector(".word-row.active");
+        const activeRowBoxes = activeRow.childNodes;
+
+        let anyBoxActive = false;
+
+        activeRowBoxes.forEach(function (box, index) {
+            if (box.classList.contains("active")) {
+                anyBoxActive = true;
+            }
+        })
+
+        if (!anyBoxActive) {
+            checkAnswer();
+        }
+    }
+
+    function checkAnswer() {
+
+    }
 
     const gameDiv = document.createElement("div");
-    gameDiv.setAttribute("id", "game");
+    gameDiv.setAttribute("id", "wordle-game");
+    document.querySelector("body").append(gameDiv);
 
+    const rowContainer = document.createElement("div");
+    rowContainer.classList.add("row-container")
+    gameDiv.append(rowContainer)
 
+    const nLetters = 5;
+
+    createKeyboard()
+
+    createNewRow()
 }
 
 function memory() { }
